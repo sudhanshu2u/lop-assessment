0b9d24bb8909ec90637f69db6011226fcb5564f3
sudhanshu2u <sudhanshu2u@gmail.com>
2026-06-11 00:24:41 +0530

    Exclude /api/setup from auth middleware

--- proxy.ts
+++ proxy.ts
@@ -26,5 +26,5 @@ export default auth((req) => {
 });
 
 export const config = {
-  matcher: ["/((?!login|api/auth|_next/static|_next/image|favicon.ico|public).*)"],
+  matcher: ["/((?!login|api/auth|api/setup|_next/static|_next/image|favicon.ico|public).*)"],
 };
